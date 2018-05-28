dda0f3464a88b1d8dbc7ff6c435919f38084fec7
Shobhit Bakliwal <shobhitic@gmail.com>
2018-05-28 15:05:13 +0530

    Removed subscription plan as it will be used later, after getting more insights on how the data will be structured.

--- app/models/subscription_plan.rb
+++
@@ -1,2 +0,0 @@
-class SubscriptionPlan < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_124532) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "subscription_plans", force: :cascade do |t|
-    t.string "name"
-    t.string "remote_provider_id"
-    t.decimal "amount", precision: 18, scale: 4
-    t.string "currency"
-    t.string "cycle"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
