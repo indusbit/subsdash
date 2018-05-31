770142361a5fd2fbddb23156440971c0e0d1edb7
Rachit Garg <rachit05garg@gmail.com>
2018-05-31 13:10:12 +0530

    [API] Removed protect_from_forgery as it's required only in development

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,5 @@
 module Api::V1
   class ApiController < ApplicationController
-    protect_from_forgery with: :null_session
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: { error: e.message }, status: :not_found
