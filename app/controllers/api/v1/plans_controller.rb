f7573e8c69e5bf689a9964e2f2af7caad3531e9a
Rachit Garg <rachit05garg@gmail.com>
2018-05-31 15:31:07 +0530

    [API] Added destroy API for plans

--- app/controllers/api/v1/plans_controller.rb
+++ app/controllers/api/v1/plans_controller.rb
@@ -8,7 +8,7 @@ module Api::V1
 
     # GET /v1/plans/:id
     def show
-      render status: :unprocessable_entity, json: { error: "Please pass plan oid" } unless params[:id]
+      return render status: :unprocessable_entity, json: { error: "Please pass plan oid" } unless params[:id]
       @plan = Plan.find_by_oid params[:id]
       
       render status: :unprocessable_entity, json: { error: "Plan not found." } unless @plan
@@ -17,7 +17,18 @@ module Api::V1
     def create
       @plan = Plan.new plan_params
 
-      render status: :unprocessable_entity, json: { error: @plan.errors } unless @plan.save 
+      render status: :unprocessable_entity, json: { error: @plan.errors.full_messages } unless @plan.save 
+    end
+
+    def destroy
+      @plan = Plan.find_by_oid params[:id]
+      return render status: :unprocessable_entity, json: { error: "Plan not found." } unless @plan
+
+      if @plan.destroy
+        render json: { message: "Successfully deleted the plan." }
+      else
+        render status: :unprocessable_entity, json: { error: @plan.errors.full_messages }
+      end
     end
 
   private
