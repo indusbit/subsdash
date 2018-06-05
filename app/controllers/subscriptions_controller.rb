aea361ca38d080de2525835b68a995eb0732c3d4
Shobhit Bakliwal <shobhitic@gmail.com>
2018-06-05 17:18:35 +0530

    Optimised subscriptions controller loading.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class SubscriptionsController < ApplicationController
   def index
-    @subscriptions = Subscription.all.order(id: :desc)
+    @subscriptions = Subscription.all.order(id: :desc).includes([:plan, :customer])
   end
 
   def create
